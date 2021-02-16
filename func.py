7e072670c74a420ee9790a5097006a512bc47d12
Santiago Hartmann <santiagohartmann97@gmail.com>
2021-02-16 16:31:05 +0100

    b

---
+++ func.py
@@ -0,0 +1,10 @@
+def clean_data(x):
+    #remove null values
+    x.dropna()
+    #remove duplicates
+    x.drop_duplicates()
+    #remove columns
+    x.drop(['club_involved_name', 'fee', 'league_name', 'season'], axis = 1 )
+
+   
+
